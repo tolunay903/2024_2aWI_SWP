dcb236ba609ae377a66b12c26c0354c849a5dbe2
Tolunay Akyildiz <tolunay.akyildiz@student.htldornbirn.at>
2025-01-13 09:59:28 +0100

    commit

---
+++ JS/index.js
@@ -0,0 +1,15 @@
+console.log("Hallo");
+
+let a = 7;
+let b = 8;
+let c = a + b;
+
+console.log("A hat den Wert" + a);
+
+a = 10;
+console.log("A hat den Wert" + a);
+
+console.log("a + b = " + c);
+
+c = b - a;
+console.log("b - a = " + c)
