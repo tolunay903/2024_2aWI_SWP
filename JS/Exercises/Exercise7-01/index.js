a2d0ec867af1c0828bb572e172aba92f59bbdb26
Tolunay Akyildiz <tolunay.akyildiz@student.htldornbirn.at>
2025-02-21 14:44:26 +0100

    commit

---
+++ JS/Exercises/Exercise7-01/index.js
@@ -0,0 +1,18 @@
+/*/Du hast folgende Eingabe "1 2 3 4" 
+
+Line 2: n space-separated integer numbers x_1 .. x_n. 
+
+Output 
+sum of even numbers 
+
+Constraints 
+-100 <= x_i <= 100 
+
+Example 
+
+Input 
+1 2 3 4 
+
+Output 
+6 
+*/
