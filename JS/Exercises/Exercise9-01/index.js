12bfa3e75578b0ae50bd84bc7e8a94551e17126d
Tolunay Akyildiz <tolunay.akyildiz@student.htldornbirn.at>
2025-03-21 22:33:30 +0100

    commit

--- JS/Exercises/Exercise9-01/index.js
+++ JS/Exercises/Exercise9-01/index.js
@@ -3,7 +3,11 @@
  of both 5 and 7 print "FooBar".*/
 
 for (i = 0; i <= 222; i++) {
-    if (i % 5 == 0) {
+    if (i % 5 == 0 && i % 7 == 0) {
+        console.log('FooBar')
+    }
+
+    else if (i % 5 == 0) {
         console.log('Foo');
     }
 
