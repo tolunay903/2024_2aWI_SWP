406a12975787e2a4cd3d14cf504d04a2cafa0413
Tolunay Akyildiz <tolunay.akyildiz@student.htldornbirn.at>
2025-05-16 13:59:42 +0200

    commit

---
+++ JS&HTML/Data03/script.js
@@ -0,0 +1,24 @@
+let scores = [
+    {
+        name: "Olaf",
+        points: 50
+    },
+    {
+        name: "Sven",
+        points: 57
+    },
+    {
+        name: "Hugo",
+        points: 43
+    }
+];
+
+function displayScores() {
+    let html = "";
+    scores.forEach(element => {
+        html += "<div class = 'scores'>" + element.name + ": " + element.points + "</div>";
+    });
+    document.getElementById("scores").innerHTML = html;
+}
+
+displayScores();
