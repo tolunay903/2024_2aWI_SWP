485130697ea3c3274c51003837a2073752c70ecf
Tolunay Akyildiz <tolunay.akyildiz@student.htldornbirn.at>
2025-05-28 10:02:26 +0200

    commit

--- JS/Exercises/Exercise11-02/index.js
+++ JS/Exercises/Exercise11-02/index.js
@@ -1,22 +1,21 @@
-let binary = [0, 0, 0, 1, 1, 1, 0, 0, 0];
+let binary = "1010010"
 
 let result = inverted(binary);
 
 console.log(result)
 
 function inverted() {
-
-    let inverted = ""
+    let inverted = "";
 
 
     for (let i = 0; i < binary.length; i++) {
         if (binary[i] === '0') {
             inverted += '1';
+
         } else if (binary[i] === '1') {
             inverted += '0';
         }
 
-        console.log(inverted);
     }
-
+    return inverted;
 }
